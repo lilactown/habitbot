e3fcdb8a8ff7153ae8e23dff542f6d83f66922d4
Will <will@willacton.com>
2015-08-19 11:23:03 -0700

    logs messages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 
 // const Slack = require('slack-client');
 import { default as Slack } from 'slack-client';
-import _ from 'lodash';
+import { map, filter } from 'lodash/collection';
+import { has } from 'lodash/object';
 import { readFileSync } from 'fs';
 
 // import api token
@@ -14,9 +15,34 @@ const autoMark = true;
 
 const slack = new Slack(token, autoReconnect, autoMark);
 
+const currentChannels = (channels) => map(
+	filter(channels, (channel, key) => channel.is_member),
+	(channel, key) => channel.name
+);
+
+const currentGroups = (groups) => map(
+	filter(groups, () => group.is_open && !group.is_archived),
+	(group, key) => group.name
+);
+
 slack.on('open', () => {
-	const channels = slack.channels;
-	console.log(channels);
+	const channels = currentChannels(slack.channels);
+	const groups = currentGroups(slack.groups);
+	const unreads = slack.getUnreadCount();
+
+	console.log(`In channels: ${channels.join(', ')}`);
+	console.log(`In groups: ${groups.join(', ')}`);
+	console.log(`Unread messages: ${unreads}`);
+});
+
+slack.on('message', (message) => {
+	const channel = slack.getChannelGroupOrDMByID(message.channel);
+	const user = slack.getUserByID(message.user);
+	const {type, ts, text} = message;
+
+	const userName = has(user, 'name') ? user.name : 'UKNNOWN_USER';
+	const channelName = channel ? channel.name : 'UKNOWN_CHANNEL';
+	console.log(`[${type}:#${channelName}] ${userName} ${ts}> ${text}`);
 });
 
 slack.on('error', (err) => console.error(...err));
