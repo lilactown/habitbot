287264af1dddabf9f8f6284a3aee47ee9788d62a
Will <will@willacton.com>
2015-08-19 10:27:28 -0700

    log channels obj

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 	Slack bot for team #ghostbusters on Habitica!
 */
 
-import * as slack from 'slack-client';
+// const Slack = require('slack-client');
+import { default as Slack } from 'slack-client';
+import _ from 'lodash';
 import { readFileSync } from 'fs';
 
 // import api token
 const token = readFileSync('apiToken', { encoding: 'utf8' });
-console.log(token);
+const autoReconnect = true;
+const autoMark = true;
+
+const slack = new Slack(token, autoReconnect, autoMark);
+
+slack.on('open', () => {
+	const channels = slack.channels;
+	console.log(channels);
+});
+
+slack.on('error', (err) => console.error(...err));
+
+// let's go
+slack.login();
