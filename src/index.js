f6e41fea38859b97c64b7e7d3d80dad0cea18c5d
Will <will@willacton.com>
2015-08-19 11:45:41 -0700

    hello command

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 
 // const Slack = require('slack-client');
 import { default as Slack } from 'slack-client';
+import { default as Promise } from 'bluebird';
 import { map, filter } from 'lodash/collection';
 import { has } from 'lodash/object';
 import { readFileSync } from 'fs';
@@ -15,15 +16,37 @@ const autoMark = true;
 
 const slack = new Slack(token, autoReconnect, autoMark);
 
-const currentChannels = (channels) => map(
-	filter(channels, (channel, key) => channel.is_member),
-	(channel, key) => channel.name
-);
+const commands = {
+	hello: (channel, userName) => {
+		const message = `Hello, ${userName}! My name is habitbot. Type "@habitbot: help" for more info.`;
+		channel.send(message);
+		console.log(`[${channel}] > ${message}`);
+	}
+};
 
-const currentGroups = (groups) => map(
-	filter(groups, () => group.is_open && !group.is_archived),
-	(group, key) => group.name
-);
+function currentChannels(channels) {
+	return map(
+		filter(channels, (channel, key) => channel.is_member),
+		(channel, key) => channel.name
+	);
+}
+
+function currentGroups(groups) {
+	return map(
+		filter(groups, () => group.is_open && !group.is_archived),
+		(group, key) => group.name
+	);
+}
+
+function isDirect(id, text) {
+	const tag = `<@${id}>`;
+	return text && text.substr(0, tag.length) === tag;
+}
+
+function stripTag(id, text) {
+	const tag=`<@${id}>: `;
+	return text.substr(tag.length);
+}
 
 slack.on('open', () => {
 	const channels = currentChannels(slack.channels);
@@ -40,9 +63,16 @@ slack.on('message', (message) => {
 	const user = slack.getUserByID(message.user);
 	const {type, ts, text} = message;
 
-	const userName = has(user, 'name') ? user.name : 'UKNNOWN_USER';
-	const channelName = channel ? channel.name : 'UKNOWN_CHANNEL';
-	console.log(`[${type}:#${channelName}] ${userName} ${ts}> ${text}`);
+	if (isDirect(slack.self.id, text)) {
+		const userName = has(user, 'name') ? user.name : 'UKNNOWN_USER';
+		const channelName = channel ? channel.name : 'UKNOWN_CHANNEL';
+		const msg = stripTag(slack.self.id, text);
+		console.log(`[${type}:#${channelName}] ${userName} ${ts}> ${msg}`);
+
+		if (msg === 'hello') {
+			commands.hello(channel, userName);
+		}
+	}
 });
 
 slack.on('error', (err) => console.error(...err));
